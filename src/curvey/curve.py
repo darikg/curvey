da807f522dce9c73a9fa01634f583d53cef9fa8b
Darik Gamble <darik.gamble@gmail.com>
2024-04-06 08:23:53 -0400

    be more explicit about orientation definition

--- src/curvey/curve.py
+++ src/curvey/curve.py
@@ -587,7 +587,7 @@ class Curve:
         Parameters
         ----------
         orientation
-            Must be either 1 or -1.
+            Must be either `+1` or `-1`. `+1' is counterclockwise.
         """
         if orientation not in (1, -1):
             msg = f"orientation must be either 1 or -1, got {orientation}"
